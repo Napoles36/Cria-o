23c7cdbdbcb04bcc7d69195777a42d2bfbc9ad29
Napoles36 <123393436+Napoles36@users.noreply.github.com>
2024-02-08 17:39:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function bodyLoad()
+{
+  
+  document.getElementById("bt").style.fontSize = "15px";
+  document.getElementById("bt").style.backgroundColor= "chartreuse";
+  
+  document.getElementById("bt2").style.fontSize = "20px";
+  document.getElementById("bt2").style.backgroundColor= "gray";
+  
+ }
+function changeTheme()
+{
+ document.getElementById("body").style.backgroundColor="white"; 
+}
+
+
+function back()
+{
+ document.getElementById("body").style.backgroundColor="chartreuse"; 
+}
+
+
+ 
